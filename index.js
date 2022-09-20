a20798abb3748dda4993ec76dbfd98f920ff539b
Paolo Garcia <pmatteo1108@yahoo.com>
2022-09-20 09:45:36 -0600

    commit tree 35426604d9a53ad488f9f85e74ee893b6e219e26

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+
+// const currentUser = "Paolo Garcia";
+
+// const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = "Welcome, ${currentUser.slice(0,1)}!";
+
+const currentUser = 'Paolo Garcia';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
